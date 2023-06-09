592eaf3f983efc3251ce143d63e74d00dbd6f513
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-06-09 13:29:19 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SingleQuestion from './Question';
+
+
+function App() {
+  return (
+    <div className="container">
+      <h3>سوالی برایتان پیش آمده؟</h3>
+      <div className="info">
+        <SingleQuestion />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+function Question() {
+     return (
+          <div className="question">
+               <header>
+                    <h4>این سوال اول است</h4>
+                    <button><AiOutlinePlus /></button>
+               </header>
+               <p>این محتوا است</p>
+          </div>
+     )
+}
+
+export default Question
