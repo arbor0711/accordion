f59804d56ea2f2555bfbe78e316e88dc13ab589b
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-06-10 11:47:51 +0200

    fix collapsible button

---
+++ src/Infobox.js
@@ -0,0 +1,10 @@
+import Question from "./Question";
+function InfoBox() {
+  return (
+    <div className="container">
+      <Question data={""} />
+    </div>
+  );
+}
+
+export default InfoBox;
--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 
-function Question() {
-     return (
-          <div className="question">
-               <header>
-                    <h4>این سوال اول است</h4>
-                    <button><AiOutlinePlus /></button>
-               </header>
-               <p>این محتوا است</p>
-          </div>
-     )
+function Question({ props }) {
+  const [showInfo, setShowInfo] = useState("false");
+  return (
+    <section className="info-box">
+      <h3>Do you have any questions?</h3>
+      <div className="question">
+        <header>
+          <h4>Question 1?</h4>
+          <span onClick={() => setShowInfo(!showInfo)}>
+            {showInfo ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle />}{" "}
+          </span>
+        </header>
+        {showInfo && (
+          <p>
+            Duis sint incididunt ea culpa eu est commodo mollit laboris fugiat.
+          </p>
+        )}
+      </div>
+    </section>
+  );
 }
 
-export default Question
+export default Question;
